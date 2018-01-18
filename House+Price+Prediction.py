6596e0158c7c5ed873635f2139d9ae40a433d0a5
Jeffrey Chan <jachan.santiago@gmail.com>
2018-01-18 01:36:29 -0400

    add prepare data step

--- House+Price+Prediction.py
+++ House+Price+Prediction.py
@@ -1,62 +1,138 @@
 
 # coding: utf-8
 
-# In[ ]:
+# In[29]:
 
 from sklearn import linear_model
 import numpy as np
 import pandas as pd
+from sklearn.preprocessing import Imputer
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import OneHotEncoder
 
-# In[78]:
+
+# ## LOAD DATA:
+
+# In[30]:
 
 # Load Data
 train_data = pd.read_csv("train.csv")
 test_data = pd.read_csv("test.csv")
 
-# train_data.head()
-# test_data.head()
+# In[35]:
 
 # Store Ids of test data for later in the csv creation to kaggle submition
 test_ids = test_data["Id"]
 
 
-# In[84]:
+# In[36]:
 
 # Load Targets
 y = train_data["SalePrice"]
 # Convert to numpy array
 y = np.array(y)
 
-print(y.shape)
+
+# ## Prepare Data for Machine Learning:
+#
+# **Don't look much the next piece of code** basicly do:
+#     - Numerical Data:
+#         Change NaN for the median of the column
+#     - Categorical Data:
+#         Change to Int
+#         Hot One encode Categorical Data
+
+# In[33]:
 
 
-# #### remember X is our training features and T our test features
+imputer = Imputer(strategy="median")
+scaler = StandardScaler()
+hot_enc = OneHotEncoder()
 
-# In[68]:
+
+def prepare_data(dataframe, feature_list=list(), training=True):
+
+    if not feature_list:
+        feature_list = list(dataframe)
+
+    dataframe = dataframe[feature_list]
+
+    # Split dataframe in numerical and categorial data
+    num_data = dataframe.select_dtypes(include=[np.number])
+    cat_data = dataframe.select_dtypes(include=[object])
+
+    if not num_data.empty:
+
+        # Replace all NaN with the median in numerial data
+        if training:
+            imputer.fit(num_data)
+        X_num = imputer.transform(num_data)
+
+        # Scale between -1, 1
+        if training:
+            scaler.fit(X_num)
+
+        X_num = scaler.transform(X_num)
+
+        # Check if have categorical data
+        if cat_data.empty:
+            return X_num
+
+    if not cat_data.empty:
+
+        # Replace all NaN with "None" as other category in categorical data
+        cat_data.fillna('None', inplace=True)
+
+        facto_cat_data = pd.DataFrame()
+
+        # Factorize each categorical column (string -> int)
+        for feature in list(cat_data):
+            facto_cat_data[feature], _ = pd.factorize(cat_data[feature])
+
+        # Hot encode
+        if training:
+            hot_enc.fit(facto_cat_data.values)
+        X_cat_1hot = hot_enc.transform(facto_cat_data.values).todense()
+
+        # Check if have numerical data
+        if num_data.empty:
+            return X_cat_1hot
+
+    # Merge Numerical Data with One Hot encoded categorical data
+    X = np.append(X_num, X_cat_1hot, axis=1)
+
+    return X
+
+
+# ## Extract our Training Features
+#
+#  ***TODO: Select Better Features***
+
+# In[40]:
 
 # Load Selected features
-X = np.array([
-                train_data["YearBuilt"],
-                train_data["YrSold"],
-                train_data["LotArea"]
-            ])
+# X = np.array([  # TODO: Select better features
+#                 train_data["YearBuilt"],
+#                 train_data["YrSold"],
+#                 train_data["LotArea"]
+#             ])
 
-T = np.array([
-                test_data["YearBuilt"],
-                test_data["YrSold"],
-                test_data["LotArea"]
-            ])
+# T = np.array([  # TODO: Select better features
+#                 test_data["YearBuilt"],
+#                 test_data["YrSold"],
+#                 test_data["LotArea"]
+#             ])
 
-# transpose for get:
-# Columns as features
-X = X.T
-T = T.T
+training_features = ["YearBuilt", "YrSold", "LotArea", "Street"]  # TODO
 
-print(X.shape)
-print(T.shape)
+# Training Data need to be True
+X = prepare_data(train_data, training_features, True)
+
+# Test Data need to be False
+T = prepare_data(test_data, training_features, False)
 
 
-# In[64]:
+# In[41]:
 
 # Training Features
 #             #TamaNo    #Cuartos
@@ -75,12 +151,32 @@ reg = linear_model.LinearRegression()
 # Training
 reg.fit(X, y)
 
-# In[83]:
+
+# In[ ]:
 
 # Predictions
 
 # Predict
 pred = reg.predict(T)
+
+
+# In[52]:
+
+# For this problem Kaggle do not accept negatives values
+# NOTE: We know negatives value are wrong
+
+# TODO Select one
+
+# Option 1: Saturate
+if False:
+    pred[pred < 0] = 0
+
+# Option 2: Absolute Value
+if True:
+    pred = np.abs(pred)
+
+
+# In[53]:
 
 # Create a "table" each index name is column name
 # Kaggle Format
@@ -90,8 +186,10 @@ df_dict = {"SalePrice": pred,
 # Convert to Pandas DataFrame
 df = pd.DataFrame(df_dict)
 
-# Show Some data
-df.head()
+
+# In[54]:
 
 # Save in to csv file
-df.to_csv("pred.csv", index=False)
+pred_filename = "predictions-with-" + "+".join(training_features) + ".csv"
+df.to_csv(pred_filename, index=False)
+print("Output file: " + pred_filename)
